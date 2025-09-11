20688fe89c4bda9246932cd1112a10d04adc312c
Powzyy <kylevach@gmail.com>
2025-09-11 08:56:10 -0400

    Week2 Exercise 2

---
+++ Week2/guess_course.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+Program name: guess_course.py
+Program purpose: 
+Author: Kyle Vachon
+Date and Version: 09/11/2025 v1.0
+Completion time: 
+"""
+
+#variables
+course_code = "CST8245"
+guessed_code = "" # initialize guessed_code to an empty string
+
+# while loop that keeps going until the user guesses right or chooses to exit 
+while True:
+    #prompt user for input
+    guessed_code = input("Press q to exit ,or Guess the course code (e.g., CST1000): ")
+
+    #if the user wants to exit then print exit message and exit the while loop
+    if guessed_code == "q":  
+        print("Exiting the program.")
+        break 
+    #if the guess is wrong then print incorrect message and itterate the while loop
+    elif course_code != guessed_code:
+        print("Incorrect course code. Please try again.")
+    #if the guess is right then print correct message and exit the while loop
+    elif guessed_code == course_code:
+        print("Congratulations! You guessed the correct course code.")
+        break
+
+#end of program message
+print("Thank you for playing!")
+
+
+
+
+
+
+
+
+
