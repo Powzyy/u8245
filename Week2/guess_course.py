c9b1029ef1867433f5732dd65b9c5b741db34ba8
Powzyy <kylevach@gmail.com>
2025-09-11 09:02:33 -0400

    Final Commit

--- Week2/guess_course.py
+++ Week2/guess_course.py
@@ -13,21 +13,20 @@ course_code = "CST8245"
 guessed_code = "" # initialize guessed_code to an empty string
 
 # while loop that keeps going until the user guesses right or chooses to exit 
-while True:
+while guessed_code != course_code:
     #prompt user for input
     guessed_code = input("Press q to exit ,or Guess the course code (e.g., CST1000): ")
 
     #if the user wants to exit then print exit message and exit the while loop
     if guessed_code == "q":  
         print("Exiting the program.")
-        break 
+        break
     #if the guess is wrong then print incorrect message and itterate the while loop
     elif course_code != guessed_code:
         print("Incorrect course code. Please try again.")
     #if the guess is right then print correct message and exit the while loop
     elif guessed_code == course_code:
         print("Congratulations! You guessed the correct course code.")
-        break
 
 #end of program message
 print("Thank you for playing!")
