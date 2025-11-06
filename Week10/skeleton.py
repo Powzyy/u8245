6996dbe9524501c4d9da5bae9a39e34af733ea7b
Powzyy <kylevach@gmail.com>
2025-11-06 10:44:02 -0500

    Week 10

---
+++ Week10/skeleton.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+Program name: skeleton.py
+Program Purpose: Skeleton program for a book database
+Author: Kyle vachon
+Date and Version: 2025-11-06 v1.0
+Completion time: 
+"""
+
+##functions
+#show the menu
+def show_menu():
+    print ("1. Add a new book" )
+    print ("2. View all books" )
+    print ("Q. Quit" )
+
+#select a menu option and have it returned
+def get_menu_option(prompt):
+    return input(prompt).strip()
+
+#select an attribute and have it returned
+def get_attribute_value(prompt):
+    return input(prompt).strip()
+
+
+#main
+option = ''
+while option != 'Q':
+    show_menu()
+    option = get_menu_option("Select an option: ").upper()
+    if option == '1':
+        print("You selected option 1: add a new book")
+    elif option == '2':
+        print("You selected option 2: view all books")
+    elif option == 'Q':
+        print("Quitting the program.")
+    else:
+        print("Invalid option, please try again.")
